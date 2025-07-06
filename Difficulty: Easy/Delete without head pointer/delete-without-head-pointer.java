721429e6d7bbe0bd8c17608e782b61a12a2780a3
Selvaragavanvsbec <selvaragavan.vsbec@gmail.com>
2025-07-06 21:43:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete without head pointer/delete-without-head-pointer.java
@@ -0,0 +1,7 @@
+class Solution {
+    void deleteNode(Node node) {
+        if (node == null || node.next == null) return;
+        node.data = node.next.data;
+        node.next = node.next.next;
+    }
+}
