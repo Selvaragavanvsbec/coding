4f7906a838e9d53b30b35d736ebb5ad3707b64c1
Selvaragavanvsbec <selvaragavan.vsbec@gmail.com>
2025-07-11 06:16:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Delete without head pointer/delete-without-head-pointer.java
+++ Difficulty: Easy/Delete without head pointer/delete-without-head-pointer.java
@@ -1,7 +1,22 @@
+/*
+class Node
+{
+    int data ;
+    Node next;
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+*/
+
+// Function to delete a node without any reference to head pointer.
 class Solution {
     void deleteNode(Node node) {
-        if (node == null || node.next == null) return;
-        node.data = node.next.data;
-        node.next = node.next.next;
+        // Your code here
+        node.data=node.next.data;
+        node.next=node.next.next;
+        
     }
 }
