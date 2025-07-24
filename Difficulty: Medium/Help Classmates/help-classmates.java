d63540f9eb977f27c73e50fea8ddd0ace41e9f06
Selvaragavanvsbec <selvaragavan.vsbec@gmail.com>
2025-07-24 19:08:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Help Classmates/help-classmates.java
@@ -0,0 +1,26 @@
+// User function Template for Java
+
+import java.util.*;
+
+class Solution {
+    public static int[] help_classmate(int arr[], int n) {
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() >= arr[i]) {
+                stack.pop();
+            }
+
+            if (stack.isEmpty()) {
+                result[i] = -1;
+            } else {
+                result[i] = stack.peek();
+            }
+
+            stack.push(arr[i]);
+        }
+
+        return result;
+    }
+}
