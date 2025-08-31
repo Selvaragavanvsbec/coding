c05c5db93403da71a67782082aa932aed9a72505
Selvaragavanvsbec <selvaragavan.vsbec@gmail.com>
2025-08-31 21:50:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Prime Number/prime-number.java
+++ Difficulty: Easy/Prime Number/prime-number.java
@@ -1,8 +1,9 @@
 class Solution {
     static boolean isPrime(int n) {
-        if(n==1||n==2){
+        if(n==1){
             return false;
         }
+        if(n == 2) return true; 
         // code here
      for(int i=2;i*i<=n;i++){
          if(n%i==0){
