5d918539ea20d2c2624bcea56d986554ca28e3a0
Selvaragavanvsbec <selvaragavan.vsbec@gmail.com>
2025-12-29 07:54:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Level order traversal/level-order-traversal.java
+++ Difficulty: Medium/Level order traversal/level-order-traversal.java
@@ -1,26 +1,37 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
+*/
 class Solution {
     public ArrayList<ArrayList<Integer>> levelOrder(Node root) {
-        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
-        if (root == null) return result;
-
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(root);
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            ArrayList<Integer> level = new ArrayList<>();
-
-            for (int i = 0; i < size; i++) {
-                Node node = queue.poll();
-                level.add(node.data);
-
-                if (node.left != null) queue.add(node.left);
-                if (node.right != null) queue.add(node.right);
-            }
-
-            result.add(level);
-        }
-
-        return result;
+       Queue<Node> q=new LinkedList<>();
+       ArrayList<ArrayList<Integer>> al=new ArrayList<>();
+    
+       if(root==null){
+           return al;
+       }
+          q.add(root);
+          while(!q.isEmpty()){
+             int num=q.size();
+             ArrayList<Integer> sub=new ArrayList<>();
+             for(int i=1;i<=num;i++){
+                 if(q.peek().left!=null){
+                     q.add(q.peek().left);
+                 }
+                  if(q.peek().right!=null){
+                     q.add(q.peek().right);
+                 }
+                 sub.add(q.poll().data);
+             }
+             al.add(sub);
+          }
+          return al;
     }
 }
