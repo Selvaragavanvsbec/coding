db39857534fdd066864def5c5f01101b20ae7c2b
Selvaragavanvsbec <selvaragavan.vsbec@gmail.com>
2025-12-31 20:21:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
@@ -13,28 +13,28 @@ class Node {
 class Solution {
     public ArrayList<Integer> topView(Node root) {
         // code here
-        TreeMap<Integer,ArrayList<Integer>>map=new TreeMap<>();
-        ArrayList<Integer> al=new ArrayList<>();
-        Queue<Node> q=new LinkedList<>();
-        Queue<Integer> hdq=new LinkedList<>();
-        q.add(root);
-        hdq.add(0);
-        while(!q.isEmpty()){
-            Node curr=q.poll();
-            int chdq=hdq.poll();
-            map.computeIfAbsent(chdq,k->new ArrayList<>()).add(curr.data);
+       TreeMap<Integer,ArrayList<Integer>>map=new TreeMap<>();
+       ArrayList<Integer> al=new ArrayList<>();
+       Queue<Node> q=new LinkedList<>();
+         Queue<Integer> hd=new LinkedList<>();
+         q.add(root);
+         hd.add(0);
+         while(!q.isEmpty()){
+             Node curr=q.poll();
+            int chd=hd.poll();
+            map.computeIfAbsent(chd,k->new ArrayList<>()).add(curr.data);
             if(curr.left!=null){
                 q.add(curr.left);
-                hdq.add(chdq-1);
+                hd.add(chd-1);
             }
              if(curr.right!=null){
                 q.add(curr.right);
-                hdq.add(chdq+1);
+                hd.add(chd+1);
             }
-        }
-        for(ArrayList<Integer> a:map.values()){
-            al.add(a.get(0));
-        }
-        return al;
+         }
+         for(ArrayList<Integer> val:map.values()){
+             al.add(val.get(0));
+         }
+         return al;
     }
 }
